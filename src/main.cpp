3981b4937eb5795a84db9cdfde845e3660c5bfb9
Madhav2005-Garg <madhavgarg545@gmail.com>
2026-02-03 09:22:00 +0530

    add monitor speed in platformio.ini file

--- src/main.cpp
+++ src/main.cpp
@@ -9,4 +9,17 @@ void setup() {
 
 void loop() {
 // write your code here
+    digitalWrite(trig_pin,HIGH);
+    delayMicroseconds(2);
+    digitalWrite(trig_pin,LOW);
+    delayMicroseconds(2);
+    digitalWrite(trig_pin,HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trig_pin,LOW);
+    long duration=pulseIn(echo_pin,HIGH);
+    long distance=0.343*duration/2;
+    serial.print("Distance: ");
+    serial.print(distance);
+    serial.print("  cm");
+    delay(1000);
 }
